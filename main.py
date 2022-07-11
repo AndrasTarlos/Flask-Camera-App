27631a8f78d487a5c7c0911c1d3b61820413a40b
AndrasTarlos <tarlosa@bzz.ch>
2022-07-11 11:22:43 +0200

    Merge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import base64
+import os
 import re
 import cv2
 import numpy
@@ -16,7 +17,6 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
-
 '''
 @socketio.on("my event")
 def test_message(message):
@@ -25,8 +25,6 @@ def test_message(message):
 @socketio.on("my broadcast event")
 def test_message(message):
     emit("my respones", {"data": message["data"]}, broadcast=True)
-'''
-
 
 @socketio.on("connect")
 def connect():
@@ -39,25 +37,25 @@ def get_face_coordinates(base64_data):
     byte_data = base64.urlsafe_b64decode(image_data)
 
     print("Hi")
-    '''
+    
     pil_img = Image.open(io.BytesIO(byte_data)).convert("RGB")
     ocv_image = numpy.array(pil_img)
     ocv_image = ocv_image[:, :, ::-1].copy()
     trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     grayscaled_img = cv2.cvtColor(ocv_image, cv2.COLOR_BGR2GRAY)
     face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-    '''
+    
 
 
 @socketio.on('message')
 def send_face_coordinates(coordinates):
     emit("receive_face_coordinates", coordinates)
 
-    '''
+   
     (x, y, w, h) = create_coordinates(img)
     emit("face_coordinates", {"x": x, "y": y, "w": w, "h": h})
-    '''
-
+    
+'''
 
 if __name__ == "__main__":
-    socketio.run(app, debug=False, host="0.0.0.0", PORT=3000)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
